fc31b06c844e9540037df194239b6ac851bdcc9d
Anastasia C <clarkanastasia@outlook.com>
2024-02-29 11:04:00 +0000

    create Book class

---
+++ Models/Book.cs
@@ -0,0 +1,31 @@
+namespace Bookish.Models;
+
+
+public class Book
+{
+    public required int BookId {get; set;}
+    public required string Title {get; set;}
+    public required string Author {get; set;}
+    public required int TotalCopies {get; set;}
+    public required int AvailableCopies {get; set;}
+
+    public bool Checkout()
+    {
+        bool success = false;
+
+        if(AvailableCopies>0)
+        {    
+            AvailableCopies -= 1;
+            success = true;
+        }
+        return success;
+    }
+
+    public void CheckIn()
+    {
+        AvailableCopies += 1;
+    }
+
+
+    
+}
