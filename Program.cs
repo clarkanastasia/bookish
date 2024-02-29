2332652a64281bc3162285284ea0bbda82353ed2
Anastasia C <clarkanastasia@outlook.com>
2024-02-29 16:19:45 +0000

    add Library to services

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Bookish.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<Library>();
 
 var app = builder.Build();
 
